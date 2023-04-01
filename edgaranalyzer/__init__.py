07e8194a652b655713faf33dc092b3a0315f916b
Adrian Gao <mingze.gao@sydney.edu.au>
2023-04-02 09:00:00 +1000

    Prepare for PyPI

--- edgaranalyzer/__init__.py
+++ edgaranalyzer/__init__.py
@@ -1,10 +1,15 @@
 import types
+import sys
 
-__version__ = "0.0.1"
+__version__ = "0.0.1rc1"
 __description__ = "Textual analysis on SEC filings from EDGAR"
 __author__ = "Mingze Gao"
 __author_email__ = "mingze.gao@sydney.edu.au"
 __url__ = "https://github.com/mgao6767/edgar-analyzer"
+
+if sys.version_info.major < 3 and sys.version_info.minor < 10:
+    print("Python3.10 or higher is required.")
+    sys.exit(1)
 
 CMD = types.SimpleNamespace()
 CMD.DOWNLOAD_INDEX = "download_index"
